4ddf86a7c8fb8d9b7fbb6ee55d1226347fdabc3a
Aki Kanellis <me@akikanellis.com>
2016-10-30 14:51:50 +0000

    Add generating for user without password

--- authenticator/src/main/java/com/akikanellis/authenticator/Generator.java
+++ authenticator/src/main/java/com/akikanellis/authenticator/Generator.java
@@ -1,7 +1,17 @@
 package com.akikanellis.authenticator;
 
+import java.util.Random;
+
 class Generator {
+    private final Random random;
     private final PasswordRepository passwordRepository;
 
-    Generator(PasswordRepository passwordRepository) {this.passwordRepository = passwordRepository; }
+    Generator(Random random, PasswordRepository passwordRepository) {
+        this.random = random;
+        this.passwordRepository = passwordRepository;
+    }
+
+    int generate(String userId) {
+        return random.nextInt();
+    }
 }
--- authenticator/src/test/java/com/akikanellis/authenticator/GeneratorTest.java
+++ authenticator/src/test/java/com/akikanellis/authenticator/GeneratorTest.java
@@ -1,14 +1,29 @@
 package com.akikanellis.authenticator;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.Random;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
 @RunWith(MockitoJUnitRunner.class)
 public class GeneratorTest {
+    @Mock private Random random;
     @Mock private PasswordRepository passwordRepository;
     private Generator generator;
 
-    @Before public void beforeEach() { generator = new Generator(passwordRepository);}
+    @Before public void beforeEach() { generator = new Generator(random, passwordRepository); }
+
+    @Test public void generatingPassword_withNoSetPasswordForUser_generatesNewPassword() {
+        when(random.nextInt()).thenReturn(1);
+
+        int password = generator.generate("user-id");
+
+        assertThat(password).isEqualTo(1);
+    }
 }

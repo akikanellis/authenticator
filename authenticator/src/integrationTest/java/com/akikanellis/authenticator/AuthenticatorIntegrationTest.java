11e6e3d181083fabfc0e17e69da6f5417f30c5a7
Aki Kanellis <me@akikanellis.com>
2016-10-30 16:52:43 +0000

    Add authenticator integration tests

--- authenticator/src/integrationTest/java/com/akikanellis/authenticator/AuthenticatorIntegrationTest.java
+++ authenticator/src/integrationTest/java/com/akikanellis/authenticator/AuthenticatorIntegrationTest.java
@@ -11,11 +11,35 @@ public class AuthenticatorIntegrationTest {
 
     @Before public void beforeEach() { authenticator = new Authenticator(3, SECONDS); }
 
+    @Test public void generatingPassword_forSameUserWithinTheTimeFrame_returnsSamePassword()
+            throws InterruptedException {
+        int firstPassword = authenticator.generatePassword("user-id");
+        SECONDS.sleep(2);
+        int secondPassword = authenticator.generatePassword("user-id");
+
+        assertThat(firstPassword).isEqualTo(secondPassword);
+    }
+
+    @Test public void generatingPassword_forSameUserOutsideOfTheTimeFrame_returnsDifferentPassword()
+            throws InterruptedException {
+        int firstPassword = authenticator.generatePassword("user-id");
+        SECONDS.sleep(4);
+        int secondPassword = authenticator.generatePassword("user-id");
+
+        assertThat(firstPassword).isNotEqualTo(secondPassword);
+    }
+
     @Test public void checkingPassword_withinTheTimeFrame_returnsTrue() throws InterruptedException {
         int password = authenticator.generatePassword("user-id");
-
         SECONDS.sleep(2);
 
         assertThat(authenticator.isPasswordValid("user-id", password)).isTrue();
+    }
+
+    @Test public void checkingPassword_outsideOfTheTimeFrame_returnsFalse() throws InterruptedException {
+        int password = authenticator.generatePassword("user-id");
+        SECONDS.sleep(4);
+
+        assertThat(authenticator.isPasswordValid("user-id", password)).isFalse();
     }
 }

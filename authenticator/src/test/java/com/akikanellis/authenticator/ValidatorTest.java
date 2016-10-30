51494708fc4851d31743b5d55c7b0b4e43414885
Aki Kanellis <me@akikanellis.com>
2016-10-30 15:44:29 +0000

    Add validating with existing missing user

--- authenticator/src/test/java/com/akikanellis/authenticator/ValidatorTest.java
+++ authenticator/src/test/java/com/akikanellis/authenticator/ValidatorTest.java
@@ -25,4 +25,20 @@ public class ValidatorTest {
 
         assertThat(isValid).isTrue();
     }
+
+    @Test public void validating_withExistingUserAndDifferentPassword_returnsFalse() {
+        when(passwordRepository.getPasswordOfUser("user-id")).thenReturn(OptionalInt.of(1));
+
+        boolean isValid = validator.validate("user-id", 15);
+
+        assertThat(isValid).isFalse();
+    }
+
+    @Test public void validating_withMissingUser_returnsFalse() {
+        when(passwordRepository.getPasswordOfUser("user-id")).thenReturn(OptionalInt.empty());
+
+        boolean isValid = validator.validate("user-id", 1);
+
+        assertThat(isValid).isFalse();
+    }
 }

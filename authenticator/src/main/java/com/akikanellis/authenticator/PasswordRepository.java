4f87dd9fb7a9a7ba0f1cea7edf04b37a3ed0b1de
Aki Kanellis <me@akikanellis.com>
2016-10-30 14:28:02 +0000

    Add returning password with user present

--- authenticator/src/main/java/com/akikanellis/authenticator/PasswordRepository.java
+++ authenticator/src/main/java/com/akikanellis/authenticator/PasswordRepository.java
@@ -1,4 +1,16 @@
 package com.akikanellis.authenticator;
 
+import java.util.Map;
+import java.util.OptionalInt;
+
 class PasswordRepository {
+    private final Map<String, Integer> expiringMap;
+
+    PasswordRepository(Map<String, Integer> expiringMap) { this.expiringMap = expiringMap; }
+
+    OptionalInt getPasswordOfUser(String userId) {
+        Integer password = expiringMap.get(userId);
+
+        return OptionalInt.of(password);
+    }
 }
--- authenticator/src/test/java/com/akikanellis/authenticator/PasswordRepositoryTest.java
+++ authenticator/src/test/java/com/akikanellis/authenticator/PasswordRepositoryTest.java
@@ -1,9 +1,30 @@
 package com.akikanellis.authenticator;
 
 import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.Map;
+import java.util.OptionalInt;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 public class PasswordRepositoryTest {
+    @Mock private Map<String, Integer> expiringMap;
     private PasswordRepository passwordRepository;
 
-    @Before public void beforeEach() { passwordRepository = new PasswordRepository(); }
+    @Before public void beforeEach() { passwordRepository = new PasswordRepository(expiringMap); }
+
+    @Test public void gettingPasswordOfUser_withExistingUser_returnsPassword() {
+        when(expiringMap.get("user-id")).thenReturn(1);
+
+        OptionalInt password = passwordRepository.getPasswordOfUser("user-id");
+
+        //noinspection OptionalGetWithoutIsPresent
+        assertThat(password.getAsInt()).isEqualTo(1);
+    }
 }

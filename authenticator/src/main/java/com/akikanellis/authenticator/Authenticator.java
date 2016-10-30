eba2a7a0fb00f6ec1afef15f4d49d8efcbba5b2b
Aki Kanellis <me@akikanellis.com>
2016-10-30 16:20:20 +0000

    Add default constructor

--- authenticator/src/main/java/com/akikanellis/authenticator/Authenticator.java
+++ authenticator/src/main/java/com/akikanellis/authenticator/Authenticator.java
@@ -1,10 +1,28 @@
 package com.akikanellis.authenticator;
 
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
 import static com.akikanellis.authenticator.Preconditions.checkNotNullOrEmpty;
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class Authenticator {
     private final Generator generator;
     private final Validator validator;
+
+    public Authenticator() {
+        ScheduledExecutorService expirer = Executors.newSingleThreadScheduledExecutor();
+        Map<String, Integer> expiringMap = new ExpiringMap<>(expirer, 30, SECONDS);
+        PasswordRepository passwordRepository = new PasswordRepository(expiringMap);
+
+        Random random = new Random();
+        SixDigitRandom sixDigitRandom = new SixDigitRandom(random);
+
+        this.generator = new Generator(sixDigitRandom, passwordRepository);
+        this.validator = new Validator(passwordRepository);
+    }
 
     Authenticator(Generator generator, Validator validator) {
         this.generator = generator;

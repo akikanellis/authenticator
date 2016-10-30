756ccfe0d9141911088dbad164280346a4f74972
Aki Kanellis <me@akikanellis.com>
2016-10-30 11:24:35 +0000

    Make expiring map implement map

--- authenticator/src/test/java/com/akikanellis/authenticator/ExpiringMapTest.java
+++ authenticator/src/test/java/com/akikanellis/authenticator/ExpiringMapTest.java
@@ -1,10 +1,18 @@
 package com.akikanellis.authenticator;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ExpiringMapTest {
-    private ExpiringMap expiringMap;
+    private ExpiringMap<String, Integer> expiringMap;
 
+    @Before public void beforeEach() { expiringMap = new ExpiringMap<>(); }
 
-    @Before public void beforeEach() { expiringMap = new ExpiringMap(); }
+    @Test public void interfaceOfExpiringMap_isMap() {
+        assertThat(expiringMap).isInstanceOf(Map.class);
+    }
 }

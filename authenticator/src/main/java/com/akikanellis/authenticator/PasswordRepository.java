45fae1f514438b32acc68156456cc3846b08f195
Aki Kanellis <me@akikanellis.com>
2016-10-30 14:20:05 +0000

    Add password repository

---
+++ authenticator/src/main/java/com/akikanellis/authenticator/PasswordRepository.java
@@ -0,0 +1,4 @@
+package com.akikanellis.authenticator;
+
+class PasswordRepository {
+}
---
+++ authenticator/src/test/java/com/akikanellis/authenticator/PasswordRepositoryTest.java
@@ -0,0 +1,9 @@
+package com.akikanellis.authenticator;
+
+import org.junit.Before;
+
+public class PasswordRepositoryTest {
+    private PasswordRepository passwordRepository;
+
+    @Before public void beforeEach() { passwordRepository = new PasswordRepository(); }
+}

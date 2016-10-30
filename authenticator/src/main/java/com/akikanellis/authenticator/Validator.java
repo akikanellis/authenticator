3a6862034dfd1a6a24b810242c68fb5de5bb81d1
Aki Kanellis <me@akikanellis.com>
2016-10-30 15:38:48 +0000

    Add validating with existing user and same password

--- authenticator/src/main/java/com/akikanellis/authenticator/Validator.java
+++ authenticator/src/main/java/com/akikanellis/authenticator/Validator.java
@@ -4,4 +4,8 @@ class Validator {
     private final PasswordRepository passwordRepository;
 
     Validator(PasswordRepository passwordRepository) { this.passwordRepository = passwordRepository; }
+
+    boolean validate(String userId, int password) {
+        return true;
+    }
 }
--- authenticator/src/test/java/com/akikanellis/authenticator/ValidatorTest.java
+++ authenticator/src/test/java/com/akikanellis/authenticator/ValidatorTest.java
@@ -1,9 +1,15 @@
 package com.akikanellis.authenticator;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.OptionalInt;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ValidatorTest {
@@ -12,5 +18,11 @@ public class ValidatorTest {
 
     @Before public void beforeEach() { validator = new Validator(passwordRepository); }
 
+    @Test public void validating_withExistingUserAndSamePassword_returnsTrue() {
+        when(passwordRepository.getPasswordOfUser("user-id")).thenReturn(OptionalInt.of(1));
 
+        boolean isValid = validator.validate("user-id", 1);
+
+        assertThat(isValid).isTrue();
+    }
 }

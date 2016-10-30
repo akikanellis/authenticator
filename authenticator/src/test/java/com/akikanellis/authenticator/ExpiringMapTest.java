1c6e8f2b4d4487ebcec368ec35c172db0a2ebea0
Aki Kanellis <me@akikanellis.com>
2016-10-30 11:17:46 +0000

    Add expiring map

---
+++ authenticator/src/test/java/com/akikanellis/authenticator/ExpiringMapTest.java
@@ -0,0 +1,10 @@
+package com.akikanellis.authenticator;
+
+import org.junit.Before;
+
+public class ExpiringMapTest {
+    private ExpiringMap expiringMap;
+
+
+    @Before public void beforeEach() { expiringMap = new ExpiringMap(); }
+}

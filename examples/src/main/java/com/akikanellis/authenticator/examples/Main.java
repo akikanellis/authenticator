d5d6c6f49250201810b2a006f8a634c6b986e61f
Aki Kanellis <me@akikanellis.com>
2016-10-31 22:46:50 +0000

    Refactor example

--- examples/src/main/java/com/akikanellis/authenticator/examples/Main.java
+++ examples/src/main/java/com/akikanellis/authenticator/examples/Main.java
@@ -8,21 +8,24 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            startOptionLoop(scanner);
+        } catch (Exception e) {
+            System.err.println("Something went wrong: " + e);
+        }
+    }
+
+    private static void startOptionLoop(Scanner scanner) {
         Authenticator authenticator = new Authenticator();
-        Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            System.out.println("Please select an option from the following:" +
+            System.out.print("Available options:" +
                     "\n1 - Password generation" +
                     "\n2 - Password verification" +
-                    "\n3 - Exit");
-            int option;
-            try {
-                String line = scanner.nextLine();
-                option = Integer.parseInt(line);
-            } catch (InputMismatchException | NumberFormatException e) {
-                option = -1;
-            }
+                    "\n3 - Exit" +
+                    "\nPlease select an option by typing its number: ");
+            int option = extractOption(scanner);
+            System.out.println();
 
             switch (option) {
                 case 1:
@@ -35,13 +38,22 @@ public class Main {
                     System.out.println("Exiting.");
                     return;
                 default:
-                    System.out.println("Unknown option was selected. Please try again.");
+                    System.out.println("Unknown option was selected. Please try again.\n");
             }
         }
     }
 
+    private static int extractOption(Scanner scanner) {
+        try {
+            String option = scanner.nextLine();
+            return Integer.parseInt(option);
+        } catch (InputMismatchException | NumberFormatException e) {
+            return -1;
+        }
+    }
+
     private static void passwordGeneration(Authenticator authenticator, Scanner scanner) {
-        System.out.println("Password generation.");
+        System.out.println("---Password generation---");
         System.out.print("Enter a user id: ");
         String userId = scanner.nextLine();
 
@@ -49,14 +61,21 @@ public class Main {
 
         System.out.printf("Password has been generated." +
                 "\nUser id: %s" +
-                "\nPassword: %d", userId, password);
+                "\nPassword: %d\n\n", userId, password);
     }
 
     private static void passwordAuthentication(Authenticator authenticator, Scanner scanner) {
-        System.out.println("Password authentication.");
+        System.out.println("---Password authentication---");
         System.out.print("Enter a user id: ");
         String userId = scanner.nextLine();
+        int password = extractPassword(scanner);
 
+        boolean valid = authenticator.isPasswordValid(userId, password);
+
+        System.out.printf("Verification %s\n\n", valid ? "succeeded" : "failed");
+    }
+
+    private static int extractPassword(Scanner scanner) {
         int password;
         do {
             System.out.print("Enter a password: ");
@@ -64,12 +83,11 @@ public class Main {
                 String line = scanner.nextLine();
                 password = Integer.parseInt(line);
             } catch (NumberFormatException e) {
+                System.out.println("This was not a valid password. Please try again.");
                 password = -1;
             }
         } while (password == -1);
 
-        boolean valid = authenticator.isPasswordValid(userId, password);
-
-        System.out.printf("Verification %s\n", valid ? "succeeded" : "failed");
+        return password;
     }
 }

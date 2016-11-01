f8c39582741e3d5c898f777d2413966caaa73f28
Aki Kanellis <aki@supenta.com>
2016-11-01 17:49:02 +0000

    Add PasswordGenerationAlgorithm documentation

--- authenticator/src/main/java/com/akikanellis/authenticator/PasswordGenerationAlgorithm.java
+++ authenticator/src/main/java/com/akikanellis/authenticator/PasswordGenerationAlgorithm.java
@@ -2,6 +2,9 @@ package com.akikanellis.authenticator;
 
 import java.util.Random;
 
+/**
+ * Generates a password for a user which is a 6-digit random integer.
+ */
 class PasswordGenerationAlgorithm {
     private static final int LOWER_BOUND = 100000;
     private static final int UPPER_BOUND = 999999;

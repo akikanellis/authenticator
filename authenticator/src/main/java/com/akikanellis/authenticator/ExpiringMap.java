5d044ddf6c66a9539a403112348007a4267d49c1
Aki Kanellis <aki@supenta.com>
2016-11-01 17:33:02 +0000

    Add ExpiringMap documentation

--- authenticator/src/main/java/com/akikanellis/authenticator/ExpiringMap.java
+++ authenticator/src/main/java/com/akikanellis/authenticator/ExpiringMap.java
@@ -7,6 +7,13 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * A Map that expires its entries after a give amount of time by using a
+ * {@link java.util.concurrent.ScheduledExecutorService}. This map is thread-safe.
+ *
+ * @param <K> the type of keys maintained by this map
+ * @param <V> the type of mapped values
+ */
 class ExpiringMap<K, V> implements Map<K, V> {
     private final Map<K, V> map;
     private final ScheduledExecutorService expirer;
@@ -30,6 +37,19 @@ class ExpiringMap<K, V> implements Map<K, V> {
 
     @Override public V get(Object key) { return map.get(key); }
 
+    /**
+     * Associates the specified value with the specified key in this map. If the map previously contained a mapping for
+     * the key, the old value is replaced by the specified value.
+     * <p>
+     * After putting it, it schedules the entry to be removed after the specified amount of time.
+     *
+     * @param key   key with which the specified value is to be associated
+     * @param value value to be associated with the specified key
+     * @return the previous value associated with key, or null if there was no mapping for the key
+     * @throws UnsupportedOperationException if the <tt>put</tt> operation is not supported by this map
+     * @throws ClassCastException            if the class of the specified key or value prevents it from being stored in
+     *                                       this map
+     */
     @Override public V put(K key, V value) {
         V previousValue = map.put(key, value);
         expirer.schedule((Runnable) () -> map.remove(key), expiringDelay, timeUnit);
